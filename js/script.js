a39cc097e93d399f91944128d3e82b8614bb4b32
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-02 10:32:31 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 //создаём массив с именами
 var names = ['Вася', 'Петя', 'Витя', 'Олег', 'Катя', 'Алексей'];
+//создадим массив с численными значениями.
+var numbers = [12, 653, 213, 65, 34, 912, 54];
 
 //создаём функцию, которая будет сортировать массив.
-function sortName(names) {
-    names.sort();
-    return names;
+function sortName(massive) {
+    massive.sort();
+    return massive;
 };
 
 //выводим результат в консоль.
 console.log(sortName(names));
+//выводим результат в консоль.
+console.log(sortName(numbers));
+//теперь мы убедились, что у нас всё работает правильно.
